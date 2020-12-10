35da6d769d6c467e182920a998a290205e3ba21c
maksimus <gorlovmaks23@gmail.com>
2020-12-10 16:54:19 +0600

    Добавила подключение к базе данных и к фб

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+    name: "events",
+    url: "mongodb://localhost"
+  },
+  fb: {
+    appId: "191679332561189",
+    appSecret: "78b7c7d694504e63681108c779291d6e"
+  }
+};
